e127c9c74975bba2ec0636a8a43384f398a32382
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2026-02-09 09:23:37 +0100

    feat: add about us header feature

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import AboutHeader from "@/features/components/AboutHeader";
+import AboutHero from "@/features/components/AboutHero";
+import CoreValues from "@/features/components/CoreValues";
+import MissionVision from "@/features/components/MissionVision";
+
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen bg-white">
+      {/* About Us Header */}
+      <AboutHeader />
+
+           {/* Additional sections will be added here */}
+    </main>
+  );
+}
