6053078dc015d49d1b6a6c32b51be79f038b7d42
Anthony Nebenmor <nebenmor.anthony@gmail.com>
2026-02-10 09:41:39 +0100

    feat: add cta section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,9 @@ import AboutHero from "@/features/components/AboutHero";
 import CoreValues from "@/features/components/CoreValues";
 import MissionVision from "@/features/components/MissionVision";
 import CEOSection from "@/features/components/CEOSection";
+import CommitmentSection from "@/features/components/CommitmentSection";
+import FAQSection from "@/features/components/FAQSection";
+import AboutCTA from "@/features/components/AboutCTA";
 
 export default function AboutPage() {
   return (
@@ -23,7 +26,17 @@ export default function AboutPage() {
       {/* CEO Section */}
       <CEOSection />
 
-      {/* Additional sections will be added here */}
+      {/* CTA Section */}
+      
+      {/* Commitment to Excellence Section */}
+      <CommitmentSection />
+
+      
+      {/* FAQ Section */}
+      <FAQSection />
+
+      {/* CTA Section */}
+      <AboutCTA />
     </main>
   );
 }
